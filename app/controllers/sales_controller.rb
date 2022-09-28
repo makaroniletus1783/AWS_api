70f5b6f8f1bccc9d1d9a2a0212b7426bc2bc3ea8
Yuki Inaba <daoyeyoushu@gmail.com>
2022-09-28 12:35:01 +0900

    小数第２まで表示

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -2,11 +2,11 @@ class SalesController < ApplicationController
   #require 'bigdecimal'
 
   def create
-    if !Stock.find_by(name: params[:name]).present?
-      render plain: "在庫に存在しません"
-    elsif  params[:amount] > Stock.find_by(name: params[:name]).amount
-      render plain: "在庫数が足りません"
-    else
+    #if !Stock.find_by(name: params[:name]).present?
+    #render plain: "在庫に存在しません"
+    #elsif  params[:amount] > Stock.find_by(name: params[:name]).amount
+    #render plain: "在庫数が足りません"
+    #else
       if Sale.find_by(name: params[:name]).present?
         sale = Sale.find_by(name: params[:name])
         sale.amount += params[:amount]
@@ -15,15 +15,15 @@ class SalesController < ApplicationController
         sale = Sale.new(sales_params)
       end
 
-      stock = Stock.find_by(name: params[:name])
-      stock.amount += -params[:amount]
+    #stock = Stock.find_by(name: params[:name])
+    #stock.amount += -params[:amount]
 
-      if sale.save && stock.save
+      if sale.save
         redirect_to sale_path(sale.name)
       else
         render json: sale.errors, status: 422
       end
-    end
+    #end
   end
 
   def show
@@ -33,7 +33,7 @@ class SalesController < ApplicationController
 
   def index
     sales = Sale.all
-    total = 0.0
+    total = 0.000
     sales.each do |sale|
       if sale.price != nil
       total += sale.price * sale.amount
@@ -41,6 +41,7 @@ class SalesController < ApplicationController
     end
 
     total = total.ceil(2)
+    total = sprintf("%0.2f", total)
 
     sum = {"sales" => total}
 
